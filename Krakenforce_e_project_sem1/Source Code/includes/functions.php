62ee570eb0266e620dc899f8adf66d143dcc96f3
samval <samvalentine2811@gmail.com>
2019-11-02 14:02:55 +0700

    them file autoloader cho classes + improve database.class

--- Krakenforce_e_project_sem1/Source Code/includes/functions.php
+++ Krakenforce_e_project_sem1/Source Code/includes/functions.php
@@ -1,2 +1,26 @@
 <?php
 // các function/ method các loại mà không muốn để trong classes
+
+
+//--------- tự load classes: -------
+spl_autoload_register('autoloader');
+
+function autoloader($class){
+
+    // $url = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+    $class = strtolower($class);
+    $path = "../classes/";
+    $extension = ".class.php";
+    $fullPath = $path . $class . $extension;
+
+
+    if (!file_exists($fullPath)) {
+        return false;
+    }else {
+
+        include_once $fullPath;
+    }
+
+}
+// -----------------

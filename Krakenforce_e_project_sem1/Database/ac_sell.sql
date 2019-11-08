9ae2bff39b77720523219664972d8e84723474a3
Toan Ngo <45133814+toan158n@users.noreply.github.com>
2019-11-08 19:47:57 +0700

    change something

--- Krakenforce_e_project_sem1/Database/ac_sell.sql
+++ Krakenforce_e_project_sem1/Database/ac_sell.sql
@@ -2,15 +2,13 @@
 -- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 07, 2019 at 05:17 AM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+-- Host: localhost:3301
+-- Generation Time: Nov 08, 2019 at 10:49 AM
+-- Server version: 5.7.26
+-- PHP Version: 7.3.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+07:00";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -43,7 +41,7 @@ CREATE TABLE `customer` (
 --
 
 CREATE TABLE `product` (
-  `pro_id` int(10) NOT NULL,
+  `pro_id` int(11) NOT NULL,
   `product_code` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `name` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
   `price` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
@@ -52,6 +50,30 @@ CREATE TABLE `product` (
   `image` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
   `download` varchar(250) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`pro_id`, `product_code`, `name`, `price`, `brand`, `type`, `image`, `download`) VALUES
+(2, '123', 'product2', '500 USD', 'SOYA', 'SPLIT', 'none', 'none'),
+(3, '123', 'product3', '500 USD', 'SOYA', 'SPLIT', 'none', 'none'),
+(4, '123', 'product3', '500 USD', 'SOYA', 'SPLIT', 'none', 'none'),
+(5, '123', 'product3', '500 USD', 'SOYA', 'SPLIT', 'none', 'none'),
+(6, '123', 'product3', '500 USD', 'SOYA', 'SPLIT', 'none', 'none'),
+(7, '123', 'product3', '500 USD', 'SOYA', 'SPLIT', 'none', 'none'),
+(8, '123', 'product3', '500 USD', 'SOYA', 'SPLIT', 'none', 'none'),
+(9, '123', 'product3', '500 USD', 'SOYA', 'SPLIT', 'none', 'none'),
+(10, '23', 'product5', '500 USD', 'LG', 'SPLIT', 'none', 'none'),
+(11, '23', 'product10', '400 USD', 'MITSUBISHI', 'SPLIT', 'none', 'none');
+
+--
+-- Triggers `product`
+--
+DELIMITER $$
+CREATE TRIGGER `create_product` AFTER INSERT ON `product` FOR EACH ROW insert into product_detail(pro_id, product_code) values(new.pro_id, new.product_code)
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -73,6 +95,22 @@ CREATE TABLE `product_detail` (
   `fea_02` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
   `fea_03` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `product_detail`
+--
+
+INSERT INTO `product_detail` (`pro_id`, `product_code`, `model`, `color`, `ton`, `cooling_cap`, `heating_cap`, `pw_input`, `eer`, `fea_01`, `fea_02`, `fea_03`) VALUES
+(2, '123', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(3, '123', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(4, '123', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(5, '123', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(6, '123', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(7, '123', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(8, '123', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(9, '123', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(10, '23', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(11, '23', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -107,21 +145,26 @@ ALTER TABLE `customer`
   MODIFY `customer_id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `pro_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- AUTO_INCREMENT for table `product_detail`
 --
 ALTER TABLE `product_detail`
-  MODIFY `pro_id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `pro_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `product`
+-- Constraints for table `product_detail`
 --
-ALTER TABLE `product`
-  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`pro_id`) REFERENCES `product_detail` (`pro_id`);
-COMMIT;
+ALTER TABLE `product_detail`
+  ADD CONSTRAINT `product_detail_ibfk_1` FOREIGN KEY (`pro_id`) REFERENCES `product` (`pro_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

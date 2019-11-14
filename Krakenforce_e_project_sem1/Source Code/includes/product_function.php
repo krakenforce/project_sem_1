4ed849ce1665605dcc0cc0cebb4a8d968a8ed576
Toan Ngo <45133814+toan158n@users.noreply.github.com>
2019-11-14 11:16:14 +0700

    xoá file thừa

--- Krakenforce_e_project_sem1/Source Code/includes/product_function.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once ('../includes/functions.php');
-    $db= new Database();
-    $query = "SELECT * FROM product INNER JOIN product_detail pd on product.pro_id = pd.pro_id";
-    $data = $db->selectData($query);
-    
-    

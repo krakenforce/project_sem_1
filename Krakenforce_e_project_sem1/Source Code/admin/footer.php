fce447251849e112e961c9b178dfd35934d7f93c
Toan Ngo <45133814+toan158n@users.noreply.github.com>
2019-11-15 23:08:24 +0700

    Update footer for login.php

--- Krakenforce_e_project_sem1/Source Code/admin/footer.php
+++ Krakenforce_e_project_sem1/Source Code/admin/footer.php
@@ -1,4 +1,14 @@
-</body>
-
-</html>
-
+<footer>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-10 col-lg-9 col-md-8 ml-auto">
+                <div class="row pt-3">
+                    <div class="col-lg-6 text-center fixed-bottom">
+                        <p>&copy; 2019 Copyright. Made<i class="fab fa-gitkraken text-danger"></i> by <span
+                                    class="text-light">Kraken Force</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>

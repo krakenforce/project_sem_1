d0c57e14fce4f484e85c1e29f84f891cbebe349c
Toan Ngo <45133814+toan158n@users.noreply.github.com>
2019-11-16 16:00:55 +0700

    upload PLEASE READ.txt and fix product_detail.php

--- Krakenforce_e_project_sem1/Source Code/admin/product_detail.php
+++ Krakenforce_e_project_sem1/Source Code/admin/product_detail.php
@@ -81,92 +81,92 @@
                             <img class="align-content-center" height="100px" width="auto"
                                  src="<?php echo $product->product_info['image']; ?>" alt="">
                             <div class="form-row">
-                                <div class="form-group col-md-2 readonly_area">
+                                <div class="form-group col-md-2 ">
                                     <label for="pro_id">Product ID:</label><br/>
                                     <input type="text" name="" id=""
                                            value="<?php echo $product->product_info['pro_id']; ?>">
                                 </div>
-                                <div class="form-group col-md-2 readonly_area">
+                                <div class="form-group col-md-2 ">
                                     <label for="pro_code">Product Code:</label><br/>
                                     <input type="text" name="" id=""
                                            value="<?php echo $product->product_info['product_code']; ?>">
                                 </div>
-                                <div class="form-group col-md-3 readonly_area">
+                                <div class="form-group col-md-3 ">
                                     <label for="name">Name:</label><br/>
                                     <input type="text" name="" id=""
                                            value="<?php echo $product->product_info['name']; ?>">
                                 </div>
-                                <div class="form-group col-md-2 readonly_area">
+                                <div class="form-group col-md-2 ">
                                     <label for="brand">Brand:</label><br/>
                                     <input type="text" name="" id=""
                                            value="<?php echo $product->product_info['brand']; ?>">
                                 </div>
-                                <div class="form-group col-md-2 readonly_area">
+                                <div class="form-group col-md-2 ">
                                     <label for="brand">Price:</label><br/>
                                     <input type="text" name="" id=""
                                            value="<?php echo $product->product_info['price']; ?>">
                                 </div>
                             </div>
                             <div class="form-row">
-                                <div class="form-group col-md-2 readonly_area">
+                                <div class="form-group col-md-2 ">
                                     <label for="model">Model:</label><br/>
                                     <input type="text" name="" id=""
                                            value="<?php echo $product->product_info['model']; ?>">
                                 </div>
-                                <div class="form-group col-md-2 readonly_area">
+                                <div class="form-group col-md-2 ">
                                     <label for="color">Color:</label><br/>
                                     <input type="text" name="" id=""
                                            value="<?php echo $product->product_info['color']; ?>">
                                 </div>
-                                <div class="form-group col-md-1 readonly_area">
+                                <div class="form-group col-md-1 ">
                                     <label for="ton">Ton:</label><br/>
                                     <input type="text" name="" id=""
                                            value="<?php echo $product->product_info['ton']; ?>">
                                 </div>
-                                <div class="form-group col-md-2 readonly_area">
+                                <div class="form-group col-md-2 ">
                                     <label for="cooling_cap">Cooling Capacity:</label><br/>
                                     <input type="text" name="" id=""
                                            value="<?php echo $product->product_info['cooling_cap']; ?>">
                                 </div>
-                                <div class="form-group col-md-2 readonly_area">
+                                <div class="form-group col-md-2 ">
                                     <label for="heating_cap">Heating Capacity:</label><br/>
                                     <input type="text" name="" id=""
                                            value="<?php echo $product->product_info['heating_cap']; ?>">
                                 </div>
-                                <div class="form-group col-md-2 readonly_area">
+                                <div class="form-group col-md-2 ">
                                     <label for="pw_input">Power Input:</label><br/>
                                     <input type="text" name="" id=""
                                            value="<?php echo $product->product_info['pw_input']; ?>">
                                 </div>
                             </div>
                             <div class="form-row">
-                                <div class="form-group col-md-2 readonly_area">
+                                <div class="form-group col-md-2 ">
                                     <label for="pw_input">Power Input:</label><br/>
                                     <input type="text" name="" id=""
                                            value="<?php echo $product->product_info['pw_input']; ?>">
                                 </div>
-                                <div class="form-group col-md-3 readonly_area">
+                                <div class="form-group col-md-3 ">
                                     <label for="fea_01">Feature 1:</label><br/>
                                     <input type="text" name="" id=""
                                            value="<?php echo $product->product_info['fea_01']; ?>">
                                 </div>
-                                <div class="form-group col-md-3 readonly_area">
+                                <div class="form-group col-md-3 ">
                                     <label for="fea_02">Feature 2:</label><br/>
                                     <input type="text" name="" id=""
                                            value="<?php echo $product->product_info['fea_02']; ?>">
                                 </div>
-                                <div class="form-group col-md-3 readonly_area">
+                                <div class="form-group col-md-3 ">
                                     <label for="fea_03">Feature 3:</label><br/>
                                     <input type="text" name="" id=""
                                            value="<?php echo $product->product_info['fea_03']; ?>">
                                 </div>
                             </div>
                             <div class="form-row">
-                                <div class="form-group col-md-3 readonly_area">
+                                <div class="form-group col-md-3 ">
                                     <label for="download">EER</label><br/>
                                     <input value="<?php echo $product->product_info['eer']; ?>">
                                 </div>
-                                <div class="form-group col-md-3 readonly_area">
+                                <div class="form-group col-md-3 ">
                                     <label for="download">Detail Information Download File</label><br/>
                                     <input value="<?php echo $product->product_info['download']; ?>">
                                 </div>
